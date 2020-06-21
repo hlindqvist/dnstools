c44d38df084d17ec1f76c691228d56d19c902aef
Håkan Lindqvist <hawk@qw.se>
2020-06-21 22:52:32 +0200

    Bump year in copyright notice

--- ddns-edit-zone.py
+++ ddns-edit-zone.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-#   Copyright 2011-2019 Hakan Lindqvist <dnstools@qw.se>
+#   Copyright 2011-2020 Hakan Lindqvist <dnstools@qw.se>
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
